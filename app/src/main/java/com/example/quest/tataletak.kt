c41b5a5dbd282a471cf134dc4d274c59c3457fd8
verss21 <arjunafrizal01@gmail.com>
2025-10-16 17:20:04 +0700

    feat: tambahkan Row dalam Box pertama pada TataletakBoxColumnRow dengan tiga komponen teks

--- app/src/main/java/com/example/quest/tataletak.kt
+++ app/src/main/java/com/example/quest/tataletak.kt
@@ -118,5 +118,16 @@ fun TataletakBoxColumnRow(modifier: Modifier){
                 .background(color = Color.Yellow),
             contentAlignment = Alignment.Center
         ) {
+            Column() {
+                Row(
+                    modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(text = "Col1_Row1_Komponen1")
+                    Text(text = "Col1_Row1_Komponen2")
+                    Text(text = "Col1_Row1_Komponen3")
+                }
+            }
+
 
 
