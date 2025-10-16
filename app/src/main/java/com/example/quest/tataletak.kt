4a2786479834830c918ce572a67bce3eae89a3e5
verss21 <arjunafrizal01@gmail.com>
2025-10-16 17:09:58 +0700

    menambahkan fungsi tataletakrow dengan empat komponen teks horizontal

--- app/src/main/java/com/example/quest/tataletak.kt
+++ app/src/main/java/com/example/quest/tataletak.kt
@@ -40,6 +40,17 @@ fun TataletakColumn(modifier: Modifier) {
     }
 }
 
+@Composable
+fun TataLetakRow(modifier : Modifier) {
+    Row(modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly) {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
+}
+
 
 
 
