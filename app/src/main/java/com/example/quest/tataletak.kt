4f1d868caccf05edccb6777ff416a72ff56ec07d
verss21 <arjunafrizal01@gmail.com>
2025-10-16 17:19:16 +0700

    menambahkan fungsi TataletakBoxColumnRow dengan struktur awal Box dan Column

--- app/src/main/java/com/example/quest/tataletak.kt
+++ app/src/main/java/com/example/quest/tataletak.kt
@@ -107,6 +107,16 @@ fun TataletakRowColumn(modifier: Modifier) {
     }
 }
 
-
+@Composable
+fun TataletakBoxColumnRow(modifier: Modifier){
+    val gambar = painterResource(id = R.drawable.notasibalok)
+    Column() {
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(height = 110.dp)
+                .background(color = Color.Yellow),
+            contentAlignment = Alignment.Center
+        ) {
 
 
