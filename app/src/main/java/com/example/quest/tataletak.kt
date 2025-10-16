adcc70cacf583cb8a6fb677699865fd8e147ff63
verss21 <arjunafrizal01@gmail.com>
2025-10-16 17:11:24 +0700

    menambahkan fungsi tataletakBox sebagai container dengan bbrp teks

--- app/src/main/java/com/example/quest/tataletak.kt
+++ app/src/main/java/com/example/quest/tataletak.kt
@@ -51,6 +51,21 @@ fun TataLetakRow(modifier : Modifier) {
     }
 }
 
+@Composable
+fun TataletakBox(modifier: Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxHeight()
+            .fillMaxWidth(), contentAlignment = Alignment.Center
+    ){
+        Text(text = "Box 1")
+        Text(text = "Column 1")
+        Text(text = "Row 1")
+        Text(text = "Box 2")
+        Text(text = "Column 2")
+    }
+}
+
 
 
 
