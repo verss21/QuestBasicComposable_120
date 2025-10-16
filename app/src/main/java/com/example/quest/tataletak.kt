e815fdbde5d2b497e7bd37fa4a9196c3488a9a60
verss21 <arjunafrizal01@gmail.com>
2025-10-16 17:17:27 +0700

    menambahkan fungsi TataletakRowColumn dengan dua kolom teks di dalam Row

--- app/src/main/java/com/example/quest/tataletak.kt
+++ app/src/main/java/com/example/quest/tataletak.kt
@@ -89,5 +89,24 @@ fun TataletakColumnRow(modifier: Modifier) {
     }
 }
 
+@Composable
+fun TataletakRowColumn(modifier: Modifier) {
+    Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+    }
+    //Kolom 1
+    Column(){
+        Text(text = "Komponen1Kolom1")
+        Text(text = "Komponen2Kolom1")
+        Text(text = "Komponen3Kolom1")
+    }
+    //Kolom 2
+    Column() {
+        Text(text = "Komponen1Kolom2")
+        Text(text = "Komponen2Kolom2")
+        Text(text = "Komponen3Kolom2")
+    }
+}
+
+
 
 
